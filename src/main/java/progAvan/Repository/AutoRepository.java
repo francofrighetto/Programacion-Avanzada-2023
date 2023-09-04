36fd99dd417084202efab8aa453f25361432cad0
FranciscoBotta <87836164+3ottA@users.noreply.github.com>
2023-09-04 19:59:40 -0300

    Auto backend

---
+++ src/main/java/progAvan/Repository/AutoRepository.java
@@ -0,0 +1,8 @@
+package progAvan.Repository;
+
+import progAvan.Model.Auto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoRepository extends JpaRepository<Auto, Long> {
+
+}
