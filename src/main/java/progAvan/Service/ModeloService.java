b17a1fdcbbc0f84e5dee1a24134f40e219f6a157
FranciscoBotta <87836164+3ottA@users.noreply.github.com>
2023-09-06 20:44:27 -0300

    Registrar Modelo

--- src/main/java/progAvan/Service/ModeloService.java
+++ src/main/java/progAvan/Service/ModeloService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import progAvan.Model.Modelo;
+import progAvan.Repository.MarcaRepository;
 import progAvan.Repository.ModeloRepository;
 
 @Service
@@ -14,6 +15,9 @@ public class ModeloService {
 
     @Autowired
     ModeloRepository modeloRepository;
+
+    @Autowired
+    MarcaRepository marcaRepository;
 
     // public void createmodelo() {
     // modeloRepository.
