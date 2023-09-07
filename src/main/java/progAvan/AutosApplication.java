5e79c6707d82a285f3afe6563a3128734feccdf9
FranciscoBotta <87836164+3ottA@users.noreply.github.com>
2023-09-07 20:46:56 -0300

    chau hibernate viejo

--- src/main/java/progAvan/AutosApplication.java
+++ src/main/java/progAvan/AutosApplication.java
@@ -3,14 +3,11 @@ package progAvan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import progAvan.Hibernate.HibernateUtil;
-
 @SpringBootApplication
 public class AutosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutosApplication.class, args);
-		HibernateUtil.inicializar();
 	}
 
 }
