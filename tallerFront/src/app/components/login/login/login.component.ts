7bb6a5487b7429faa0c5014aa6dd79d9d5bc63ef
valentinolopezbruno <valentinolopezbruno@outlook.com>
2023-09-22 10:29:04 -0300

    Front Loggin tino x cofal 22/09

--- tallerFront/src/app/components/login/login/login.component.ts
+++ tallerFront/src/app/components/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  code:{} = {};
+  nombre  = '';
+  contra = '';
 
-  ngOnInit(): void {
-  }
+  /* constructor(private APIService: APIService){} */
+
+  guardarEnLocalStorage(token:string) {
+   try {
+     localStorage.setItem("token", JSON.stringify(token));
+   } catch (error) {
+     console.error('Error al guardar en el localStorage:', error);
+   }
+ }
+
+ validarCodigo(data:any):any{
+   if(data.code === 1){
+     var {token, code} = data;
+     this.guardarEnLocalStorage(token);
+      Swal.fire({
+       title: 'Ingresaste Correctamente',
+       icon: 'success'
+     });
+
+     window.location.href = `https://misseguidores.com/admin`
+   } else{
+     return Swal.fire({
+       title: 'Datos Incorrectos',
+       icon: 'error'
+     });
+   }
+
+ }
+
+/*  validarUsuario(nombre: string, contra: string){
+    this.APIService.validarUsuario(nombre, contra).subscribe((data) => {
+    this.validarCodigo(data)})
+ } */
+
+/*  iniciarSesion(){
+   this.validarUsuario(this.nombre, this.contra) 
+   console.log(this.nombre , this.contra )
+ } */
+
+ ngOnInit(): void {
+    
+ }
 
 }
--- tallerFront/src/app/components/login/login/login.module.ts
+++ tallerFront/src/app/components/login/login/login.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+
+
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -22,7 +25,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule // Agrega FormsModule a la lista de imports
   ],
   exports:[RouterModule]
 })
-export class LoginModule { }
+export class LoginModule { 
+}
