3499cd44589f95ab71314203e360ab4f73cc36cf
francofrighetto <68360116+francofrighetto@users.noreply.github.com>
2023-12-13 17:47:30 -0300

    fecha busqueda estadistica

--- src/main/java/progAvan/Controller/EstadisticaController.java
+++ src/main/java/progAvan/Controller/EstadisticaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,9 +31,30 @@ public class EstadisticaController {
     String path;
 
     @CrossOrigin(origins = { "http://localhost:4200" }, maxAge = 3600)
-    @GetMapping(value = "/comparacionMinutos")
-    public List<Object> comparacionMinutosServicios() {
-        return estadisticaService.comparacionMinutosServicios();
+    @GetMapping(value = "/comparacionMinutos/{fechaInferior}/{fechaSuperior}")
+    public List<Object> comparacionMinutosServicios(@PathVariable String fechaInferior, @PathVariable String fechaSuperior) {
+        // String whereInferior=" ";
+        // String whereConsulta;
+
+        // if (!fechaInferior.equals("no")){
+        //     System.out.println("ajsjasjs");
+        //     whereInferior= "  fecha_inicio>='"+fechaInferior+"' ";
+        // }
+        // String whereSuperior=" ";
+        // if (!fechaSuperior.equals("no")){
+        //     whereSuperior= "  fecha_inicio<='"+fechaSuperior+"' ";
+        // }
+        // whereConsulta= whereInferior + whereSuperior;
+        // if (whereInferior.equals(" ") && whereSuperior.equals(" ")){
+        //     whereConsulta = " ";
+        // }else{
+        //      whereConsulta = " where "+whereConsulta;
+
+        // }
+        // System.out.println(whereConsulta);
+        //         System.out.println(estadisticaService.comparacionMinutosServicios(whereConsulta));
+
+        return estadisticaService.comparacionMinutosServicios(fechaInferior,fechaSuperior);
     }
     
     @CrossOrigin(origins = { "http://localhost:4200" }, maxAge = 3600)
--- src/main/java/progAvan/Repository/EstadisticaRepository.java
+++ src/main/java/progAvan/Repository/EstadisticaRepository.java
@@ -10,16 +10,19 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface EstadisticaRepository extends JpaRepository<Estadistica, Long> {
-
+@Transactional
+    @Modifying
     @Query(value = "SELECT nombre, avg(minutos_realizados) as promedio_minutos_realizados, minutosestimados " +
             "FROM detalle_orden_trabajo as det " +
             "LEFT JOIN servicio as ser ON servicio_id = ser.id " +
-            " where servicio_id is not null "+
-            "GROUP BY nombre, minutosestimados", nativeQuery = true)
-    List<Object> comparacionMinutosServicios();
+            "left join orden_trabajo ON orden_trabajo.id = det.orden_id "+
+            " where fecha_inicio >= TO_TIMESTAMP(:fechaInferior, 'YYYYMMDD') and fecha_inicio <= TO_TIMESTAMP(:fechaSuperior, 'YYYYMMDD') " +
+            " GROUP BY nombre, minutosestimados;", nativeQuery = true)
+    List<Object> comparacionMinutosServicios(String fechaInferior, String fechaSuperior);
 
     @Query(value = "SELECT SUM(CASE WHEN estado = true THEN 1 ELSE 0 END) AS noTerminado, SUM(CASE WHEN estado = false THEN 1 ELSE 0 END) AS terminado,  SUM(total) AS total  FROM orden_trabajo;", nativeQuery = true)
     List<Object> estadisticaOrden();
